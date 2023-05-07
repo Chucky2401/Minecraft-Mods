1d9410bec471580f42d78fd6da9a61b531857dad
Chucky2401 <chuckydu24@gmail.com>
2023-05-07 23:41:25 +0200

    fix(Tjvs.Minecraft): fix pattern for download url for Optifine

--- inc/modules/Tjvs.Minecraft/Public/func_Get-InfoOptifine.ps1
+++ inc/modules/Tjvs.Minecraft/Public/func_Get-InfoOptifine.ps1
@@ -40,7 +40,7 @@ function Get-InfoOptifine {
 
     If ($null -ne $oFirstInfo) {
         $sFileName        = [Regex]::Escape($oFirstInfo.FileName)
-        $sPatternDownload = "^.+'(downloadx\?f=$($sFileName).+)' onclick=.+>Download<\/a>"
+        $sPatternDownload = "^.+'(downloadx\?f=$($sFileName)).+' onclick=.+>Download<\/a>"
     
         $oWebReponse = Invoke-WebRequest $oFirstInfo.DownloadUrl -WebSession $oSession -UserAgent "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"
     
